8b5472f817da40a05dc518432ef6a02beb34cd0f
yout name <sdhj@mail.ru>
2020-11-21 16:41:34 +0300

    Leetcode easy

---
+++ src/easy/LowestCommonAncestorBST.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class LowestCommonAncestorBST {
+    public static void main(String[] args) {
+
+    }
+
+    // Runtime: O(depth(lca)), Memory: O(1)
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else return root;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
