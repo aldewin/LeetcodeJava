416d465befac0913c8405b18373bbf56367ece41
yout name <sdhj@mail.ru>
2020-11-23 00:53:35 +0300

    Leetcode easy

---
+++ src/easy/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * 70. Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] += dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+}
